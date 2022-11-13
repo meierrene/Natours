82b1f867847190b55ed097a884fd9e4e2d414fe0
meierrene <renemeier.de@gmail.com>
2022-11-13 20:51:27 -0300

    Make it better

--- server.js
+++ server.js
@@ -35,3 +35,8 @@ process.on('unhandledRejection', (err) => {
   console.log(err.name, err.message);
   server.close(() => process.exit(1));
 });
+
+process.on('SIGTERM', () => {
+  console.log('👍 SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => console.log('💥 Process terminated!'));
+});
