448829e526c5f41096f47bcfcb4e879dd3203db6
meierrene <renemeier.de@gmail.com>
2022-11-13 23:54:38 -0300

    Make it better

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -4,7 +4,6 @@ const User = require('../models/userModel');
 const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
-const { events } = require('../models/tourModel');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour
@@ -60,7 +59,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 
 const createBookingCheckout = async (session) => {
-  const tour = client_reference_id;
+  const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.line_items[0].price_data.unit_amount / 100;
   await Booking.create({ tour, user, price });
